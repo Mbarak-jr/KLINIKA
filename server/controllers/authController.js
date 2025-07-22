6221d0a1f9e0154d1ba3c6030d84465ac2379df6
Mbarak-jr <mohammedmbarak77@gmail.com>
2025-07-22 20:26:00 +0300

    updated cookie setup from Lax to None

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -19,7 +19,7 @@ const sendTokenResponse = (res, user) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'Lax',
+    sameSite: 'None',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
